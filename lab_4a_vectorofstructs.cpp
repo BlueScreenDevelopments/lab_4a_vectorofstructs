55d3c72d651275b2d2e22f9febe606a11ab6e153
Brandon J <bsjulao@gmail.com>
2025-09-07 15:56:51 -0700

    Commit

--- lab_4a_vectorofstructs.cpp
+++ lab_4a_vectorofstructs.cpp
@@ -20,11 +20,40 @@ struct Color {
 };
 
 //Generate random integer
-int randoomInRange (mt19937& rng, int lo, int hi){
+int randomInRange (mt19937& rng, int lo, int hi){
     uniform_int_distribution<int> dist(lo, hi);
     return dist(rng);
 }
 
 //Generate random color
+Color randomColor(mt19937& rng){
+    uniform_int_distribution<int> byte(0,255);
+    return Color{ byte(rng), byte(rng), byte(rng)};
+}
 
-Color randomColor(mt19937& rng)
+int main(){
+    random_device rd;
+    mt19937 rng(rd());
+
+
+    //Empty Vector
+    vector<Color> colors;
+
+    //Generate random number between 25/50
+    int n = randomInRange(rng, 25, 50);
+    colors.reserve(n);
+
+
+    //Fill the table with Random Colors
+    for (int i = 0; i < n; ++i){
+        colors.push_back(randomColor(rng));
+    }
+
+    //Formatted Table
+    const int wIndex = 5;
+    const int wCOl = 6;
+
+
+    
+
+}
